40c7b5267a3ff9a2fa2946aa3fb8220f77b26c38
Jake Hawkin <forget_it_99@hotmail.co.uk>
2016-09-25 22:51:38 +0100

    Test

--- src/WebApp/wwwroot/app/DashboardController.js
+++ src/WebApp/wwwroot/app/DashboardController.js
@@ -4,6 +4,6 @@
     angular.module('app.dashboardController', [])
         .controller("dashboardController", function () {
             var vm = this;
-            vm.title = 'This is the dashboard';
+            vm.title = 'This is the Dashboard';
         })
 })();
